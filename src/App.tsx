ae085a5136f2f430d9d1275d7dbfb06e416a185c
Yongjun <wangyj641@gmail.com>
2024-02-04 10:52:30 +0800

    Add sideMenu & Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { Header, Footer } from "./components";
+import { Header, Footer, SideMenu, Carousel } from "./components";
+import { Row, Col } from "antd";
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
+      <div className={styles['page-content']}>
+        <Row style={{ marginTop: 20 }}>
+          <Col span={6}>
+            <SideMenu />
+          </Col>
+          <Col span={18}>
+            <Carousel />
+          </Col>
+        </Row>
+      </div>
       <Footer />
     </div>
   );
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
-export * from './footer'
-export * from './header'
+export * from "./footer";
+export * from "./header";
+export * from "./sideMenu";
+export * from "./carousel";
