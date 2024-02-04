9a323e5ef1e50c960b7043123abbe0a8793eb9ed
Yongjun <wangyj641@gmail.com>
2024-02-04 11:44:40 +0800

    Add production list

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { Header, Footer, SideMenu, Carousel } from "./components";
-import { Row, Col } from "antd";
+import { Header, Footer, Carousel, SideMenu, ProductCollection } from "./components";
+import { Row, Col, Typography } from "antd";
+import { productList1, productList2, productList3 } from "./mockups";
+import sideImage from './assets/images/sider_2019_12-09.png';
+import sideImage2 from './assets/images/sider_2019_02-04.png';
+import sideImage3 from './assets/images/sider_2019_02-04-2.png';
 
 function App() {
   return (
     <div className={styles.App}>
       <Header />
-      <div className={styles['page-content']}>
+      {/* ~{R3CfDZH]~} content */}
+      <div className={styles["page-content"]}>
         <Row style={{ marginTop: 20 }}>
           <Col span={6}>
             <SideMenu />
@@ -16,6 +21,33 @@ function App() {
             <Carousel />
           </Col>
         </Row>
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="warning">
+              Most recommended
+            </Typography.Title>
+          }
+          sideImage={sideImage}
+          products={productList1}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="danger">
+              Latest
+            </Typography.Title>
+          }
+          sideImage={sideImage2}
+          products={productList2}
+        />
+        <ProductCollection
+          title={
+            <Typography.Title level={3} type="success">
+              Domestic
+            </Typography.Title>
+          }
+          sideImage={sideImage3}
+          products={productList3}
+        />
       </div>
       <Footer />
     </div>
