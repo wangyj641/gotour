5c426b109c528d7e1a86843b00cdc4e9a2304d34
Yongjun <wangyj641@gmail.com>
2024-02-04 20:03:49 +0800

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,16 @@
 import React from 'react';
 import styles from "./App.module.css";
-import { Header, Footer, Carousel, SideMenu, ProductCollection, BusinessPartners } from "./components";
-import { Row, Col, Typography } from "antd";
-import { productList1, productList2, productList3 } from "./mockups";
-import sideImage from './assets/images/sider_2019_12-09.png';
-import sideImage2 from './assets/images/sider_2019_02-04.png';
-import sideImage3 from './assets/images/sider_2019_02-04-2.png';
+import { HomePage } from "./pages";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header />
-      <div className={styles["page-content"]}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              Most recommended
-            </Typography.Title>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              Latest
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              Domestic
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
-        <BusinessPartners />
-      </div>
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={HomePage} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
