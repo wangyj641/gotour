2a62d7b31163ef4221b5e03430432916a4803b03
Yongjun <wangyj641@gmail.com>
2024-02-08 11:18:15 +0800

    remove mock file

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import languageReducer from "./language/languageReducer";
 import recommendProductsReducer from "./recommendProducts/recommendProductsReducer";
-//import { thunk } from "redux-thunk";
 import { actionLog } from "./middlewares/actionLog";
 import { configureStore, Tuple } from "@reduxjs/toolkit";
 
@@ -9,8 +8,6 @@ const rootReducer = combineReducers({
   language: languageReducer,
   recommendProducts: recommendProductsReducer,
 });
-
-//const store = createStore(rootReducer, applyMiddleware(thunk, actionLog));
 
 const store = configureStore({
   reducer: rootReducer,
