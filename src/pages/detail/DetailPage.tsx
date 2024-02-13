4565dcce8149dc05f00aa388e760642f65a67193
Yongjun <wangyj641@gmail.com>
2024-02-13 22:59:44 +0800

    clearn the code

--- src/pages/detail/DetailPage.tsx
+++ src/pages/detail/DetailPage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-//import { RouteComponentProps } from "react-router";
 
 interface MatchParams {
   touristRouteId: string;
@@ -8,8 +7,6 @@ interface MatchParams {
 export const DetailPage: React.FC<any> = (
   props
 ) => {
-  //   console.log(props.history);
-  //   console.log(props.location);
   //   console.log(props.match);
   //return <h1>Tour ID: {props.match.params.touristRouteId}</h1>;
   return <h1>Detail Tour ID</h1>;
