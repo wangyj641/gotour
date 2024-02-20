c843a6bc8615bffe1f64468e57af374d83cb044a
Yongjun <wangyj641@gmail.com>
2024-02-20 21:13:56 +0800

    Implement login UI

--- src/pages/signIn/SignInPage.tsx
+++ src/pages/signIn/SignInPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { UserLayout } from "../../layouts/userLayout";
+import { SignInForm } from "./SignInForm";
 
 export const SignInPage: React.FC = (props) => {
   return (
     <UserLayout>
-      <h1>Login</h1>
+      <SignInForm />
     </UserLayout>
   );
 };
