03174dac91be998d3b31883a3c083cfede02ffd9
Yongjun <wangyj641@gmail.com>
2024-02-20 20:54:59 +0800

    Implement register logic

--- src/pages/register/RegisterForm.tsx
+++ src/pages/register/RegisterForm.tsx
@@ -1,5 +1,7 @@
 import { Form, Input, Button, Checkbox } from "antd";
 import styles from "./RegisterForm.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const layout = {
   labelCol: { span: 8 },
@@ -10,8 +12,20 @@ const tailLayout = {
 };
 
 export const RegisterForm = () => {
-  const onFinish = (values: any) => {
+  const navigate = useNavigate();
+
+  const onFinish = async (values: any) => {
     console.log("Success:", values);
+    try {
+      await axios.post("http://123.56.149.216:8080/auth/register", {
+        email: values.username,
+        password: values.password,
+        confirmPassword: values.confirm,
+      });
+      navigate("/signIn/");
+    } catch (error) {
+      alert("Register failed, please try again later");
+    }
   };
 
   const onFinishFailed = (errorInfo: any) => {
@@ -54,7 +68,7 @@ export const RegisterForm = () => {
               if (!value || getFieldValue("password") === value) {
                 return Promise.resolve();
               }
-              return Promise.reject("Password is inconsistent ！");
+              return Promise.reject("Password is inconsistent!");
             },
           }),
         ]}
